597be6af4cb01fc93609db7faac7b4c8fb1b0f3c
Andrew Osborn <andrewosborn101@gmail.com>
2019-05-14 09:54:23 -0500

    win page complete

--- client/src/pages/Win/index.js
+++ client/src/pages/Win/index.js
@@ -30,10 +30,21 @@ class Win extends Component {
             <div id="triangle-down">
             <h1>ScAvengers</h1>
             <h2>You Win! </h2>
-            <img className="firework" src="https://i.gifer.com/VZvx.gif" alt="firework"/>
+            </div>
+            <div className="row gifRow">
+              <div className="col-4">
+                <img className="gauntlet1"src="https://media.giphy.com/media/fnpxKUa2yhrUabpxdC/giphy.gif" alt="Infinity Gauntlet"/>
+              </div>
+              <div className="col-4">
+                <img className="firework" src="https://i.gifer.com/VZvx.gif" alt="firework"/>
+              </div>
+              <div className="col-4">
+                <img className="gauntlet2"src="https://media.giphy.com/media/26VjwfLk7CRslWHSWI/giphy.gif" alt="Infinity Gauntlet2"/>
+              </div>
             </div>
           </header>
           <h3 className="gameCompleted"><i>"title of game"</i> completed!</h3>
+          <h4 className="universeSaved"> You Saved the Universe!</h4>
         </div>
       </div>
     );
